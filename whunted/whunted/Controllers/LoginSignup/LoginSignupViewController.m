133fa1b744668b9edbe2ebf0accc3c4b606d5d3c
thomas nguyen <whunted.com@gmail.com>
2015-06-15 19:53:59 +0800

    change activity indicator to MBProgressHUD

--- whunted/whunted/Controllers/LoginSignup/LoginSignupViewController.m
+++ whunted/whunted/Controllers/LoginSignup/LoginSignupViewController.m
@@ -8,27 +8,32 @@
 
 #import "LoginSignupViewController.h"
 #import "MainViewController.h"
+#import "AppConstant.h"
 
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
 #import <FBSDKCoreKit/FBSDKGraphRequest.h>
 #import <Parse/Parse.h>
+#import <MBProgressHUD.h>
 
 @interface LoginSignupViewController ()
 
-@property (nonatomic, strong) UIActivityIndicatorView *activityLogin;
-@property (nonatomic, strong) UIButton *loginButton;
+@property (nonatomic, strong) UIButton *_FBLoginButton;
+@property (nonatomic, strong) UIButton *_emailLoginButton;
 
 @end
 
 @implementation LoginSignupViewController
+
+@synthesize _FBLoginButton;
+@synthesize _emailLoginButton;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     [self addBackgroundImage];
-    [self addFacebookLoginButton];
-    [self addActivityIndicatorView];
+    [self addFacebookLoginOrSignupButton];
+    [self addEmailLoginOrSignupButton];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,36 +44,42 @@
 - (void) addBackgroundImage
 {
     UIImage *originalBackground = [UIImage imageNamed:@"background_image.jpg"];
-    UIImage *blurredBackground = [self blur:originalBackground];
-    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:blurredBackground];
+    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:originalBackground];
     [self.view addSubview:backgroundImageView];
 }
 
-- (void) addFacebookLoginButton
+- (void) addFacebookLoginOrSignupButton
 {
-    CGSize winSize = [[UIScreen mainScreen] bounds].size;
-    UIImage *loginImage = [UIImage imageNamed:@"facebook_login_button.png"];
-    self.loginButton = [[UIButton alloc] init];
-    [self.loginButton setBackgroundImage: loginImage forState:UIControlStateNormal];
-    self.loginButton.frame = CGRectMake(winSize.width/2 - loginImage.size.width/2, winSize.height/2 - loginImage.size.height/2, loginImage.size.width, loginImage.size.height);
-    [self.loginButton addTarget:self action:@selector(loginButtonTouchHandler) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:self.loginButton];
+    _FBLoginButton = [[UIButton alloc] initWithFrame:CGRectMake(WINSIZE.width * 0.05, WINSIZE.height * 0.75, WINSIZE.width * 0.9, WINSIZE.height * 0.1)];
+    [_FBLoginButton setBackgroundColor:[UIColor colorWithRed:45.0/255 green:68.0/255 blue:134.0/255 alpha:1.0]];
+    [_FBLoginButton setTitle:@"Sign up or Log in with Facebook" forState:UIControlStateNormal];
+    [_FBLoginButton.titleLabel setFont:[UIFont systemFontOfSize:16]];
+    [_FBLoginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_FBLoginButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    _FBLoginButton.layer.cornerRadius = 5;
+    [_FBLoginButton addTarget:self action:@selector(loginButtonTouchHandler) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_FBLoginButton];
 }
 
-- (void) addActivityIndicatorView
+- (void) addEmailLoginOrSignupButton
 {
-    CGSize windowSize = [[UIScreen mainScreen] bounds].size;
-    self.activityLogin = [[UIActivityIndicatorView alloc] init];
-    self.activityLogin.frame = CGRectMake(windowSize.width/2 - 25, windowSize.height/2 - 25, 50, 50);
-    [self.view addSubview:self.activityLogin];
+    _emailLoginButton = [[UIButton alloc] initWithFrame:CGRectMake(WINSIZE.width * 0.05, WINSIZE.height * 0.87, WINSIZE.width * 0.9, WINSIZE.height * 0.1)];
+    [_emailLoginButton setBackgroundColor:[UIColor colorWithRed:235.0/255 green:235.0/255 blue:235.0/255 alpha:1.0]];
+    [_emailLoginButton setTitle:@"Sign up or Log in with Email" forState:UIControlStateNormal];
+    [_emailLoginButton.titleLabel setFont:[UIFont systemFontOfSize:16]];
+    [_emailLoginButton setTitleColor:[UIColor colorWithRed:60.0/255 green:60.0/255 blue:60.0/255 alpha:1.0] forState:UIControlStateNormal];
+    [_emailLoginButton setTitleColor:[UIColor colorWithRed:1.0 green:100.0/255 blue:100.0/255 alpha:1.0] forState:UIControlStateHighlighted];
+    _emailLoginButton.layer.cornerRadius = 5;
+//    [_emailLoginButton addTarget:self action:@selector(loginButtonTouchHandler) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_emailLoginButton];
 }
 
 #pragma mark - Event Handling
 
 - (void) loginButtonTouchHandler
 {
-    [self.loginButton setEnabled:YES];
-    [self.activityLogin startAnimating];
+    [_FBLoginButton setEnabled:YES];
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     [self loginOrSignUpWithFacebook];
 }
 
@@ -109,8 +120,8 @@
 
 - (void) userDidTryToLogin
 {
-    [self.loginButton setEnabled:YES];
-    [self.activityLogin stopAnimating];
+    [_FBLoginButton setEnabled:YES];
+    [MBProgressHUD hideHUDForView:self.view animated:YES];
 }
 
 - (void) presentMainViewController
@@ -226,34 +237,6 @@
     }
     
     return [NSArray arrayWithObjects:specificAddress, country, nil];
-}
-
-- (UIImage*) blur:(UIImage*)theImage
-{
-    // ***********If you need re-orienting (e.g. trying to blur a photo taken from the device camera front facing camera in portrait mode)
-    // theImage = [self reOrientIfNeeded:theImage];
-    
-    // create our blurred image
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CIImage *inputImage = [CIImage imageWithCGImage:theImage.CGImage];
-    
-    // setting up Gaussian Blur (we could use one of many filters offered by Core Image)
-    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
-    [filter setValue:inputImage forKey:kCIInputImageKey];
-    [filter setValue:[NSNumber numberWithFloat:5.0f] forKey:@"inputRadius"];
-    CIImage *result = [filter valueForKey:kCIOutputImageKey];
-    
-    // CIGaussianBlur has a tendency to shrink the image a little,
-    // this ensures it matches up exactly to the bounds of our original image
-    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
-    
-    UIImage *returnImage = [UIImage imageWithCGImage:cgImage];//create a UIImage for this function to "return" so that ARC can manage the memory of the blur... ARC can't manage CGImageRefs so we need to release it before this function "returns" and ends.
-    CGImageRelease(cgImage);//release CGImageRef because ARC doesn't manage this on its own.
-    
-    return returnImage;
-    
-    // *************** if you need scaling
-    // return [[self class] scaleIfNeeded:cgImage];
 }
 
 @end
