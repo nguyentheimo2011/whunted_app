55b3d2fc02b933428b393ff3537f08bd3f02d36e
thomas nguyen <whunted.com@gmail.com>
2015-06-16 15:43:15 +0800

    Add Firebase SDK by CocoaPods and add required frameworks

--- whunted/Pods/Target Support Files/Pods/Pods-environment.h
+++ whunted/Pods/Target Support Files/Pods/Pods-environment.h
@@ -60,6 +60,12 @@
 #define COCOAPODS_VERSION_MINOR_CLImageEditor_Core 1
 #define COCOAPODS_VERSION_PATCH_CLImageEditor_Core 4
 
+// Firebase
+#define COCOAPODS_POD_AVAILABLE_Firebase
+#define COCOAPODS_VERSION_MAJOR_Firebase 2
+#define COCOAPODS_VERSION_MINOR_Firebase 3
+#define COCOAPODS_VERSION_PATCH_Firebase 1
+
 // Haneke
 #define COCOAPODS_POD_AVAILABLE_Haneke
 #define COCOAPODS_VERSION_MAJOR_Haneke 1
