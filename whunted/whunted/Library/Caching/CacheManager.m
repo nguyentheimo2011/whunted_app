090e0ab0d70491603a4f0e4af4b4bfbe7c4e21d7
thomas nguyen <whunted.com@gmail.com>
2015-06-26 19:13:14 +0800

    add CacheManager

---
+++ whunted/whunted/Library/Caching/CacheManager.m
@@ -0,0 +1,13 @@
+//
+//  CacheManager.m
+//  whunted
+//
+//  Created by thomas nguyen on 26/6/15.
+//  Copyright (c) 2015 Whunted. All rights reserved.
+//
+
+#import "CacheManager.h"
+
+@implementation CacheManager
+
+@end
