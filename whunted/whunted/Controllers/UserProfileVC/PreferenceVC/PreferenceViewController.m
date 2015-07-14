e1553f84952c9c1756322e4333052fc64da70ebb
thomas nguyen <whunted.com@gmail.com>
2015-07-14 19:49:59 +0800

    add table view to preference VC

--- whunted/whunted/Controllers/UserProfileVC/PreferenceVC/PreferenceViewController.m
+++ whunted/whunted/Controllers/UserProfileVC/PreferenceVC/PreferenceViewController.m
@@ -8,12 +8,16 @@
 
 #import "PreferenceViewController.h"
 #import "Utilities.h"
+#import "AppConstant.h"
 
 @interface PreferenceViewController ()
 
 @end
 
 @implementation PreferenceViewController
+{
+    UITableView     *_preferenceTableView;
+}
 
 //------------------------------------------------------------------------------------------------------------------------------
 - (void) viewDidLoad
@@ -22,6 +26,7 @@
     [super viewDidLoad];
     
     [self customizeUI];
+    [self addPreferenceTableView];
 }
 
 //------------------------------------------------------------------------------------------------------------------------------
@@ -39,6 +44,68 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [Utilities customizeTitleLabel:@"Preferences" forViewController:self];
+    
+    [[UILabel appearanceWhenContainedIn:[UITableViewHeaderFooterView class], nil] setFont:[UIFont fontWithName:REGULAR_FONT_NAME size:17]];
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) addPreferenceTableView
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    _preferenceTableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    _preferenceTableView.delegate = self;
+    _preferenceTableView.dataSource = self;
+    [self.view addSubview:_preferenceTableView];
+}
+
+#pragma mark - UITableView Datasource methods
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return 4;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return 1;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    
+    return cell;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    if (section == 0)
+        return @"Where are you travelling to?";
+    else if (section == 1)
+        return @"Where are you residing at?";
+    else if (section == 2)
+        return @"What do you like to buy?";
+    else if (section == 3)
+        return @"What do you like to sell?";
+    else
+        return @"";
+}
+
+#pragma mark - UITableView delegate methods
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return 1.0f;
 }
 
 @end
