87ef805fd955c4ef2ec20cad20a042fe69463e1d
thomas nguyen <whunted.com@gmail.com>
2015-07-28 17:13:29 +0800

    add settingsTableVC

---
+++ whunted/whunted/Controllers/UserProfileVC/Settings/SettingsTableVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableVC.h
+//  whunted
+//
+//  Created by thomas nguyen on 28/7/15.
+//  Copyright (c) 2015 Whunted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableVC : UITableViewController
+
+@end
