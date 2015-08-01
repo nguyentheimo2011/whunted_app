3648a5b3a6864b6b2a3e73d4d6511b6871b6cba7
thomas nguyen <whunted.com@gmail.com>
2015-08-01 17:54:28 +0800

    complete adding rating image view

--- whunted/whunted/Controllers/Feedback/FeedbackTableViewCell/FeedbackTableViewCell.m
+++ whunted/whunted/Controllers/Feedback/FeedbackTableViewCell/FeedbackTableViewCell.m
@@ -12,11 +12,14 @@
 #import <JTImageButton.h>
 
 #define kProfilePictureWidth        40.0f
-#define kProfilePictureLeftMargin   10.0f
+#define kProfilePictureMargin       10.0f
+
+#define kRatingContainerWidth       40.0f
 
 @implementation FeedbackTableViewCell
 {
     UIImageView     *_userProfilePicture;
+    UIImageView     *_ratingImageView;
     UITextView      *_commentTextView;
     UIButton        *_writerUsernameButton;
     
@@ -31,11 +34,12 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     
     if (self) {
-//        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
         
         [self addUserProfilePicture];
-//        [self addFeedbackCommentTextView];
         [self addWriterUsernameLabel];
+        [self addFeedbackCommentTextView];
+        [self addRatingImageView];
     }
     
     return self;
@@ -47,9 +51,7 @@
 - (void) addUserProfilePicture
 //-------------------------------------------------------------------------------------------------------------------------------
 {
-    CGFloat const kPictureTopMargin = 10.0f;
-    
-    _userProfilePicture = [[UIImageView alloc] initWithFrame:CGRectMake(kProfilePictureLeftMargin, kPictureTopMargin, kProfilePictureWidth, kProfilePictureWidth)];
+    _userProfilePicture = [[UIImageView alloc] initWithFrame:CGRectMake(kProfilePictureMargin, kProfilePictureMargin, kProfilePictureWidth, kProfilePictureWidth)];
     _userProfilePicture.layer.cornerRadius = kProfilePictureWidth / 2.0;
     _userProfilePicture.backgroundColor = MAIN_BLUE_COLOR;
     [self addSubview:_userProfilePicture];
@@ -59,13 +61,13 @@
 - (void) addWriterUsernameLabel
 //-------------------------------------------------------------------------------------------------------------------------------
 {
-    CGFloat const kUsernameLabelLeftMargin = kProfilePictureWidth + 2 * kProfilePictureLeftMargin - 5;
+    CGFloat const kUsernameLabelLeftMargin = kProfilePictureWidth + 2 * kProfilePictureMargin;
     CGFloat const kUsernameLabelTopMargin = 0.0f;
     
     _writerUsernameButton = [[UIButton alloc] initWithFrame:CGRectMake(kUsernameLabelLeftMargin, kUsernameLabelTopMargin, 0, 0)];
     _writerUsernameButton.titleLabel.font = [UIFont fontWithName:REGULAR_FONT_NAME size:SMALL_FONT_SIZE];
     [_writerUsernameButton setTitle:@"nguyentheimo2011" forState:UIControlStateNormal];
-    [_writerUsernameButton setTitle:@"2011" forState:UIControlStateHighlighted];
+    [_writerUsernameButton setTitle:@"nguyentheimo2011" forState:UIControlStateHighlighted];
     [_writerUsernameButton setTitleColor:TEA_ROSE_COLOR forState:UIControlStateNormal];
     [_writerUsernameButton setTitleColor:LIGHTEST_GRAY_COLOR forState:UIControlStateHighlighted];
     [_writerUsernameButton sizeToFit];
@@ -77,25 +79,46 @@
 - (void) addFeedbackCommentTextView
 //-------------------------------------------------------------------------------------------------------------------------------
 {
-    CGFloat const kTextViewLeftMargin = kProfilePictureWidth + 2 * kProfilePictureLeftMargin - 5;
-    CGFloat const kTextViewTopMargin = 5.0f;
+    CGFloat const kTextViewLeftMargin = kProfilePictureWidth + 2 * kProfilePictureMargin;
+    CGFloat const kTextViewTopMargin = _writerUsernameButton.frame.size.height - 5;
     CGFloat const kTextViewBottomMargin = 10.0f;
-    CGFloat const kTextViewWidth = WINSIZE.width - 2 * kTextViewLeftMargin;
+    CGFloat const kTextViewWidth = WINSIZE.width - kTextViewLeftMargin - kRatingContainerWidth;
+    CGFloat const kTextViewTopInset = -10.0f;
+    CGFloat const KTextViewBottomInset = -5.0f;
+    CGFloat const kTextViewLeftInset = -5.0f;
     
     _commentTextView = [[UITextView alloc] initWithFrame:CGRectMake(kTextViewLeftMargin, kTextViewTopMargin, kTextViewWidth, 0)];
     _commentTextView.text = @"Excellent seller! Will certainly deal again in future.";
     _commentTextView.font = [UIFont fontWithName:REGULAR_FONT_NAME size:SMALL_FONT_SIZE];
+    _commentTextView.contentInset = UIEdgeInsetsMake(kTextViewTopInset, kTextViewLeftInset, KTextViewBottomInset, 0);
     [_commentTextView sizeToFit];
+    _commentTextView.frame = CGRectMake(kTextViewLeftMargin, kTextViewTopMargin, kTextViewWidth, _commentTextView.frame.size.height + kTextViewTopInset + KTextViewBottomInset);
     [self addSubview:_commentTextView];
     
     _cellHeight = kTextViewTopMargin + _commentTextView.frame.size.height + kTextViewBottomMargin;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------
+- (void) addRatingImageView
+//-------------------------------------------------------------------------------------------------------------------------------
+{
+    CGFloat const kRatingImageViewMargin = 10.0f;
+    CGFloat const kRatingImageViewXPos = WINSIZE.width - (kRatingContainerWidth - kRatingImageViewMargin);
+    CGFloat const kRatingImageViewWidth = kRatingContainerWidth - 2 * kRatingImageViewMargin;
+    
+    _ratingImageView = [[UIImageView alloc] initWithFrame:CGRectMake(kRatingImageViewXPos, kProfilePictureMargin, kRatingImageViewWidth, kRatingImageViewWidth)];
+    UIImage *ratingImage = [UIImage imageNamed:@"smiling_face.png"];
+    [_ratingImageView setImage:ratingImage];
+    [self addSubview:_ratingImageView];
+}
+
+#pragma mark - Event Handler
+
+//-------------------------------------------------------------------------------------------------------------------------------
 - (void) usernameButtonTapEventHandler
 //-------------------------------------------------------------------------------------------------------------------------------
 {
-    NSLog(@"usernameButtonTapEventHandler");
+    
 }
 
 @end
