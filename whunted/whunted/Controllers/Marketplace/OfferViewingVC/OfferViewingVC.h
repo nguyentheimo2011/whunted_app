dccb52b21050f51e03a55df7eb7a9d6dedad9824
thomas nguyen <whunted.com@gmail.com>
2015-09-07 10:48:17 +0800

    add OfferViewingVC

---
+++ whunted/whunted/Controllers/Marketplace/OfferViewingVC/OfferViewingVC.h
@@ -0,0 +1,17 @@
+//
+//  OfferViewingVC.h
+//  whunted
+//
+//  Created by thomas nguyen on 7/9/15.
+//  Copyright (c) 2015 Whunted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChatView.h"
+
+@interface OfferViewingVC : UIViewController <UITableViewDataSource, UITableViewDelegate, ChatViewDelegate>
+
+
+
+@end
---
+++ whunted/whunted/Controllers/Marketplace/OfferViewingVC/OfferViewingVC.m
@@ -0,0 +1,230 @@
+//
+//  OfferViewingVC.m
+//  whunted
+//
+//  Created by thomas nguyen on 7/9/15.
+//  Copyright (c) 2015 Whunted. All rights reserved.
+//
+
+#import "OfferViewingVC.h"
+#import "MessageViewCell.h"
+#import "AppConstant.h"
+#import "Utilities.h"
+#import "converter.h"
+#import <Firebase/Firebase.h>
+#import <MBProgressHUD.h>
+
+#define     kControlContainerHeight         60.0f
+
+@implementation OfferViewingVC
+{
+    UITableView             *_offersTableView;
+    Firebase                *_firebase;
+    
+    NSMutableArray          *_recentTransactionalMessages;
+}
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (id) init
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    self = [super init];
+    if (self != nil)
+    {
+        _recentTransactionalMessages = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) viewDidLoad
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    [super viewDidLoad];
+    
+    [Utilities customizeTitleLabel:NSLocalizedString(@"Chat", nil) forViewController:self];
+    
+    [self addSegmentedControl];
+    [self addInboxTableView];
+    
+    [self loadRecents];
+    
+    [_offersTableView registerNib:[UINib nibWithNibName:@"MessageViewCell" bundle:nil] forCellReuseIdentifier:@"MessageViewCell"];
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) addSegmentedControl
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    CGFloat const kContainerYPos = [Utilities getHeightOfNavigationAndStatusBars:self];
+    UIView *segmentContainer = [[UIView alloc] initWithFrame:CGRectMake(0, kContainerYPos, WINSIZE.width, kControlContainerHeight)];
+    [segmentContainer setBackgroundColor:[UIColor whiteColor]];
+    [self.view addSubview:segmentContainer];
+    
+    CGFloat const kSeparatorLineLeftMargin = 15.0f;
+    CGFloat const kSeparatorLineHeight = 0.75f;
+    CGFloat const kSeparatorLineWidth = WINSIZE.width - kSeparatorLineLeftMargin;
+    CGFloat const kSeparatorLineYPos = kControlContainerHeight - kSeparatorLineHeight;
+    UIView *separatorLine = [[UIView alloc] initWithFrame:CGRectMake(kSeparatorLineLeftMargin, kSeparatorLineYPos, kSeparatorLineWidth, kSeparatorLineHeight)];
+    [separatorLine setBackgroundColor:CELL_SEPARATOR_GRAY_COLOR];
+    [segmentContainer addSubview:separatorLine];
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) addInboxTableView
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    CGFloat const kTableHeight = WINSIZE.height - [Utilities getHeightOfNavigationAndStatusBars:self] - [Utilities getHeightOfBottomTabBar:self] - kControlContainerHeight;
+    
+    CGFloat const kTableYPos = kControlContainerHeight + [Utilities getHeightOfNavigationAndStatusBars:self];
+    
+    _offersTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, kTableYPos, WINSIZE.width, kTableHeight)];
+    [_offersTableView setBackgroundColor:LIGHTEST_GRAY_COLOR];
+    _offersTableView.delegate = self;
+    _offersTableView.dataSource = self;
+    [self.view addSubview:_offersTableView];
+}
+
+#pragma mark - Backend methods
+//------------------------------------------------------------------------------------------------------------------------------
+- (void)loadRecents
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    PFUser *user = [PFUser currentUser];
+    if ((user != nil) && (_firebase == nil))
+    {
+        [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+        
+        _firebase = [[Firebase alloc] initWithUrl:[NSString stringWithFormat:@"%@/Recent", FIREBASE]];
+        FQuery *query = [[_firebase queryOrderedByChild:FB_SELF_USER_ID] queryEqualToValue:user.objectId];
+        [query observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot)
+         {
+             [_recentTransactionalMessages removeAllObjects];
+             if (snapshot.value != [NSNull null])
+             {
+                 NSArray *sorted = [[snapshot.value allValues] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2)
+                                    {
+                                        NSDictionary *recent1 = (NSDictionary *)obj1;
+                                        NSDictionary *recent2 = (NSDictionary *)obj2;
+                                        NSDate *date1 = String2Date(recent1[FB_TIMESTAMP]);
+                                        NSDate *date2 = String2Date(recent2[FB_TIMESTAMP]);
+                                        return [date2 compare:date1];
+                                    }];
+                 
+                 for (NSDictionary *recent in sorted)
+                 {
+                     [_recentTransactionalMessages addObject:recent];
+                 }
+             }
+             
+             [_offersTableView reloadData];
+             [MBProgressHUD hideHUDForView:self.view animated:YES];
+         }];
+    }
+}
+
+
+#pragma mark - UITableView Datasource Delegate methods
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return [_recentTransactionalMessages count];
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    NSString *cellIdentifier = @"MessageViewCell";
+    MessageViewCell *cell = (MessageViewCell *) [_offersTableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    [cell customizeUI];
+    [cell bindData:_recentTransactionalMessages[indexPath.row]];
+    
+    return cell;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return 116.0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 1.0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    return 1.0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    view.tintColor = LIGHTEST_GRAY_COLOR;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) tableView:(UITableView *)tableView willDisplayFooterView:(UIView *)view forSection:(NSInteger)section
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    view.tintColor = LIGHTEST_GRAY_COLOR;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//------------------------------------------------------------------------------------------------------------------------------
+{
+    [_offersTableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSDictionary *recent    =   _recentTransactionalMessages[indexPath.row];
+    ChatView *chatView      =   [[ChatView alloc] initWith:recent[FB_GROUP_ID]];
+    chatView.user2Username  =   recent[FB_OPPOSING_USER_USERNAME];
+    chatView.delegate       =   self;
+    chatView.isUnread       =   [recent[FB_UNREAD_MESSAGES_COUNTER] integerValue] > 0;
+    
+    TransactionData *offerData      =   [[TransactionData alloc] init];
+    offerData.objectID              =   recent[FB_CURRENT_OFFER_ID];
+    offerData.itemID                =   recent[FB_ITEM_ID];
+    offerData.itemName              =   recent[FB_ITEM_NAME];
+    offerData.sellerID              =   [(NSArray *) recent[FB_GROUP_MEMBERS] objectAtIndex:0];
+    offerData.buyerID               =   [(NSArray *) recent[FB_GROUP_MEMBERS] objectAtIndex:1];
+    offerData.initiatorID           =   recent[FB_TRANSACTION_LAST_USER];
+    offerData.originalDemandedPrice =   recent[FB_ORIGINAL_DEMANDED_PRICE];
+    offerData.offeredPrice          =   recent[FB_CURRENT_OFFERED_PRICE];
+    offerData.deliveryTime          =   recent[FB_CURRENT_OFFERED_DELIVERY_TIME];
+    offerData.transactionStatus     =   recent[FB_TRANSACTION_STATUS];
+    
+    [chatView setOfferData:offerData];
+    
+    chatView.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:chatView animated:YES];
+}
+
+
+#pragma mark - ChatViewDelegate methods
+
+//------------------------------------------------------------------------------------------------------------------------------
+- (void) chatView:(ChatView *)chatView didSeeAnUnreadConversation:(BOOL)isUnread
+//------------------------------------------------------------------------------------------------------------------------------
+{
+
+}
+
+
+#pragma mark - Event Handlers
+
+
+
+@end
+
