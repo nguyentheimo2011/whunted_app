010242177800f00c7a5887ae976f47f779261884
thomas nguyen <whunted.com@gmail.com>
2015-10-13 18:41:08 +0800

    move chat helpers to ChatHelper folder

---
+++ whunted/whunted/Library/Utilities/BackendUtil.h
@@ -0,0 +1,13 @@
+//
+//  BackendUtil.h
+//  whunted
+//
+//  Created by thomas nguyen on 13/10/15.
+//  Copyright © 2015 Whunted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BackendUtil : NSObject
+
+@end
---
+++ whunted/whunted/Library/Utilities/BackendUtil.m
@@ -0,0 +1,13 @@
+//
+//  BackendUtil.m
+//  whunted
+//
+//  Created by thomas nguyen on 13/10/15.
+//  Copyright © 2015 Whunted. All rights reserved.
+//
+
+#import "BackendUtil.h"
+
+@implementation BackendUtil
+
+@end
