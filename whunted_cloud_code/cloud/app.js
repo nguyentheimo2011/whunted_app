ab8753729a166f1281d84fe577877e7a542cd4d4
thomas nguyen <whunted.com@gmail.com>
2015-10-30 02:26:25 +0800

    add parse module to cloud code

--- whunted_cloud_code/cloud/app.js
+++ whunted_cloud_code/cloud/app.js
@@ -2,6 +2,8 @@
 express = require('express');
 app = express();
 
+
+
 app.set('views', 'cloud/views');  
 app.set('view engine', 'ejs');    
 app.use(express.bodyParser());    
@@ -11,7 +13,15 @@ app.get('/', function(req, res) {
 });
 
 app.post('/forgotPassword', function(req, res) {
-	res.send({'email': req.body.email});
+	// res.send({'email': req.body.email});
+	Parse.User.logIn(username, password, {
+      success: function(user) {
+         res.send('success');
+      },
+      error: function(user, error) {
+        res.send('failure');
+      }
+   });
 });
 
 app.listen();
