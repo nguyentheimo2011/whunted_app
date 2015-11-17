ab4a6a2105561d16db1741d0b286552f568437ab
thomas nguyen <whunted.com@gmail.com>
2015-11-18 02:55:26 +0800

    add two labels to PhoneVerificationVC

--- whunted/whunted/Controllers/Verification/PhoneVerification/VerificationCodeRequesterVC.m
+++ whunted/whunted/Controllers/Verification/PhoneVerification/VerificationCodeRequesterVC.m
@@ -11,12 +11,17 @@
 #import "AppConstant.h"
 
 @implementation VerificationCodeRequesterVC
+{
+    UILabel             *_phoneVerificationLabel;
+    UILabel             *_verificationsBenefitsLabel;
+}
 
 //-----------------------------------------------------------------------------------------------------------------------------
 - (void) viewDidLoad
 //-----------------------------------------------------------------------------------------------------------------------------
 {
     [self customizeUI];
+    [self loadUI];
 }
 
 
@@ -30,6 +35,51 @@
     [Utilities customizeTitleLabel:NSLocalizedString(@"Phone Verification", nil) forViewController:self];
     
     self.view.backgroundColor = [UIColor whiteColor];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+- (void) loadUI
+//-----------------------------------------------------------------------------------------------------------------------------
+{
+    [self addPhoneVerficationLabel];
+    [self addVerificationsBenefitsLabel];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+- (void) addPhoneVerficationLabel
+//-----------------------------------------------------------------------------------------------------------------------------
+{
+    _phoneVerificationLabel = [[UILabel alloc] init];
+    _phoneVerificationLabel.text = NSLocalizedString(@"PHONE VERIFICATION", nil);
+    _phoneVerificationLabel.textColor = TEXT_COLOR_DARK_GRAY;
+    _phoneVerificationLabel.font = [UIFont fontWithName:SEMIBOLD_FONT_NAME size:DEFAULT_FONT_SIZE];
+    [_phoneVerificationLabel sizeToFit];
+    
+    CGFloat kLabelOriginX   =   (WINSIZE.width - _phoneVerificationLabel.frame.size.width) / 2;
+    CGFloat kLabelOriginY   =   [Utilities getHeightOfNavigationAndStatusBars:self] + 20.0f;
+    _phoneVerificationLabel.frame = CGRectMake(kLabelOriginX, kLabelOriginY, _phoneVerificationLabel.frame.size.width, _phoneVerificationLabel.frame.size.height);
+    [self.view addSubview:_phoneVerificationLabel];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+- (void) addVerificationsBenefitsLabel
+//-----------------------------------------------------------------------------------------------------------------------------
+{
+    CGFloat kLabelOriginX   =   40.0f;
+    CGFloat kLabelOriginY   =   _phoneVerificationLabel.frame.origin.y + _phoneVerificationLabel.frame.size.height + 5.0f;
+    CGFloat kLabelWidth     =   WINSIZE.width - 2 * kLabelOriginX;
+    
+    _verificationsBenefitsLabel = [[UILabel alloc] initWithFrame:CGRectMake(kLabelOriginX, kLabelOriginY, kLabelWidth, 0)];
+    _verificationsBenefitsLabel.text = NSLocalizedString(@"Increase trust between you and other buyers and sellers with phone verification", nil);
+    _verificationsBenefitsLabel.textColor = TEXT_COLOR_LESS_DARK;
+    _verificationsBenefitsLabel.font = [UIFont fontWithName:SEMIBOLD_FONT_NAME size:15];
+    _verificationsBenefitsLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _verificationsBenefitsLabel.numberOfLines = 0;
+    [_verificationsBenefitsLabel sizeToFit];
+    _verificationsBenefitsLabel.frame = CGRectMake(kLabelOriginX, kLabelOriginY, kLabelWidth, _verificationsBenefitsLabel.frame.size.height);
+    _verificationsBenefitsLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_verificationsBenefitsLabel];
+    
 }
 
 
